b4a91894271df74719a13b173f71ab4c47978d95
Isabel <isabel.antonia.seidl@gmail.com>
2020-04-03 00:25:21 +0200

    feat: modal to create quiz

--- src/App.js
+++ src/App.js
@@ -18,12 +18,6 @@ class App extends Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.setTaggedEventDate = this.setTaggedEventDate.bind(this);
-    this.transformCalendarDate = this.transformCalendarDate.bind(this);
-    this.transformCalendarDateToDate = this.transformCalendarDateToDate.bind(
-      this
-    );
-    this.setDueDate = this.setDueDate.bind(this);
     this.showModal = this.showModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
     this.controlRefetch = this.controlRefetch.bind(this);
@@ -34,20 +28,6 @@ class App extends Component {
     this.setState({
       refetch: boolean
     })
-  }
-
-  // in all modals user picks a due date from calendar
-  setDueDate(date) {
-    this.setState({
-      dueDate: date,
-    });
-  }
-
-  // in edit and create new quiz modals user picks a taggedEventDate from calendar
-  setTaggedEventDate(date) {
-    this.setState({
-      taggedEventDate: date,
-    });
   }
 
   // on TestComponent
@@ -66,7 +46,11 @@ class App extends Component {
 
   // after i fetched again component did update and i want to set triggerRefetch back to false
   componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.state.triggerRefetch) this.setState({ triggerRefetch: false });
+
+    if (this.state.refetch) {
+      this.setState({ refetch: false });
+    }
+
   }
 
   handleChange(event) {
@@ -78,49 +62,6 @@ class App extends Component {
       [name]: value,
     });
   }
-
-  // tranforms dueDate selected in calendar to Date Format for db like '2020-03-24' work then this.sendInout
-  transformCalendarDateToDate(str) {
-    let months = {
-      Jan: "01",
-      Feb: "02",
-      Mar: "03",
-      Apr: "04",
-      May: "05",
-      Jun: "06",
-      Jul: "07",
-      Aug: "08",
-      Sep: "09",
-      Oct: "10",
-      Nov: "11",
-      Dec: "12",
-    };
-
-    let parts = str.split(" ");
-    return parts[3] + "-" + months[parts[1]] + "-" + parts[2];
-  }
-
-  // transforms taggedEventDate selected in calendar to String like '24.03.2020'
-  transformCalendarDate(str) {
-    let months = {
-      Jan: "01",
-      Feb: "02",
-      Mar: "03",
-      Apr: "04",
-      May: "05",
-      Jun: "06",
-      Jul: "07",
-      Aug: "08",
-      Sep: "09",
-      Oct: "10",
-      Nov: "11",
-      Dec: "12",
-    };
-
-    let parts = str.split(" ");
-    return parts[2] + "." + months[parts[1]] + "." + parts[3];
-  }
-
 
   render() {
 
@@ -158,7 +99,7 @@ class App extends Component {
           </div>
             {/*create new quiz modal*/}
           {creating &&
-          <CreateModal controlModal={this.closeModal}></CreateModal>}
+          <CreateModal triggerRefetch={this.controlRefetch} controlModal={this.closeModal}></CreateModal>}
           {/*TestSection*/}
           <div className="testSection">
             <div className="wrapTestsToday">
@@ -170,7 +111,7 @@ class App extends Component {
             <div className="wrapTests">
               <p className="heading">All quizes:</p>
               {/*TestComponent constrolls show of CompletedModal; CompletedModal controlls close*/}
-              <TestComponent triggerRefetch={this.controlRefetch} controlModal={this.showModal}></TestComponent>
+              <TestComponent  controlModal={this.showModal}></TestComponent>
             </div>
           </div>
         </div>
--- src/CreateModal.js
+++ src/CreateModal.js
@@ -17,24 +17,83 @@ class CreateModal extends React.Component {
     this.closeModal = this.closeModal.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.postQuiz = this.postQuiz.bind(this);
+    this.transformCalendarDate = this.transformCalendarDate.bind(this);
+    this.transformCalendarDateToDate = this.transformCalendarDateToDate.bind(this);
+    this.setTaggedEventDate = this.setTaggedEventDate.bind(this);
+    this.setDueDate = this.setDueDate.bind(this);
   }
 
   handleChange(e) {
     const name = e.target.name;
-    const value = e.target.value
-      this.setState({
-          [name]: value
-      })
-
+    const value = e.target.value;
+    this.setState({
+      [name]: value,
+    });
   }
 
-    postQuiz(e) {
-    // const dueDate = this.transformCalendarDateToDate(
-    //   String(this.state.dueDate)
-    // );
-    // const taggedEventDate = this.transformCalendarDate(
-    //   String(this.state.taggedEventDate)
-    // );
+  // tranforms dueDate selected in calendar to Date Format for db like '2020-03-24' work then this.postQuiz
+  transformCalendarDateToDate(str) {
+    let months = {
+      Jan: "01",
+      Feb: "02",
+      Mar: "03",
+      Apr: "04",
+      May: "05",
+      Jun: "06",
+      Jul: "07",
+      Aug: "08",
+      Sep: "09",
+      Oct: "10",
+      Nov: "11",
+      Dec: "12",
+    };
+
+    let parts = str.split(" ");
+    return parts[3] + "-" + months[parts[1]] + "-" + parts[2];
+  }
+
+  // transforms taggedEventDate selected in calendar to String like '24.03.2020'
+  transformCalendarDate(str) {
+    let months = {
+      Jan: "01",
+      Feb: "02",
+      Mar: "03",
+      Apr: "04",
+      May: "05",
+      Jun: "06",
+      Jul: "07",
+      Aug: "08",
+      Sep: "09",
+      Oct: "10",
+      Nov: "11",
+      Dec: "12",
+    };
+
+    let parts = str.split(" ");
+    return parts[2] + "." + months[parts[1]] + "." + parts[3];
+  }
+
+    // in all modals user picks a due date from calendar
+  setDueDate(date) {
+    this.setState({
+      dueDate: date,
+    });
+  }
+
+  // in edit and create new quiz modals user picks a taggedEventDate from calendar
+  setTaggedEventDate(date) {
+    this.setState({
+      taggedEventDate: date,
+    });
+  }
+
+  postQuiz(e) {
+    const dueDate = this.transformCalendarDateToDate(
+      String(this.state.dueDate)
+    );
+    const taggedEventDate = this.transformCalendarDate(
+      String(this.state.taggedEventDate)
+    );
 
     fetch("/sendTest", {
       method: "POST",
@@ -43,9 +102,9 @@ class CreateModal extends React.Component {
       },
       body: JSON.stringify({
         name: this.state.nameOfTest,
-        dueDate: '2020-04-15',
+        dueDate: dueDate,
         taggedEvent: this.state.taggedEvent,
-        taggedEventDate: '03.04.2020',
+        taggedEventDate: taggedEventDate,
         lastTaken: this.state.lastTaken,
         status: this.state.statusOfTest,
       }),
@@ -54,11 +113,11 @@ class CreateModal extends React.Component {
       nameOfTest: "",
       taggedEvent: "",
       taggedEventDate: "",
-      dueDate: ""
+      dueDate: "",
     });
-    this.props.controlModal(false)
-        console.log(this.props.controlModal(false))
-    console.log(this.props.triggerRefetch(true))
+
+    this.props.controlModal(false);
+    this.props.triggerRefetch(true);
   }
 
   closeModal(event) {
@@ -66,7 +125,6 @@ class CreateModal extends React.Component {
   }
 
   render() {
-
     return (
       <div className="completedModal">
         <i onClick={this.closeModal} className="material-icons cross">
@@ -83,16 +141,15 @@ class CreateModal extends React.Component {
           value={this.state.nameOfTest}
           onChange={this.handleChange}
         />
-
-        {/*    <label style={{ fontSize: "1.6rem" }}>Initially due:</label>*/}
-        {/*    <DatePicker*/}
-        {/*      selected={this.state.dueDate}*/}
-        {/*      onChange={this.setDueDate}*/}
-        {/*      name="dueDate"*/}
-        {/*      placeholderText="Click to select a date"*/}
-        {/*      dateFormat="dd.MM.yyyy"*/}
-        {/*      autoComplete="off"*/}
-        {/*    />*/}
+        <label style={{ fontSize: "1.6rem" }}>Initially due:</label>
+        <DatePicker
+          selected={this.state.dueDate}
+          onChange={this.setDueDate}
+          name="dueDate"
+          placeholderText="Click to select a date"
+          dateFormat="dd.MM.yyyy"
+          autoComplete="off"
+        />
         <label style={{ fontSize: "1.6rem" }}>Relevant event:</label>
         <input
           autoComplete="off"
@@ -102,21 +159,20 @@ class CreateModal extends React.Component {
           value={this.state.taggedEvent}
           onChange={this.handleChange}
         />
-        {/*    <label style={{ fontSize: "1.6rem" }}>*/}
-        {/*      Date of relevant event:*/}
-        {/*    </label>*/}
-        {/*    <DatePicker*/}
-        {/*      selected={this.state.taggedEventDate}*/}
-        {/*      onChange={this.setTaggedEventDate}*/}
-        {/*      name="taggedEventDate"*/}
-        {/*      placeholderText="Click to select a date"*/}
-        {/*      dateFormat="dd.MM.yyyy"*/}
-        {/*      autoComplete="off"*/}
-        {/*    />*/}
+        <label style={{ fontSize: "1.6rem" }}>Date of relevant event:</label>
+        <DatePicker
+          selected={this.state.taggedEventDate}
+          onChange={this.setTaggedEventDate}
+          name="taggedEventDate"
+          placeholderText="Click to select a date"
+          dateFormat="dd.MM.yyyy"
+          autoComplete="off"
+        />
         <button
-          onClick={() => {
-            this.postQuiz();
-          }}
+          onClick={this.postQuiz}
+          // onClick={() => {
+          //   this.postQuiz();
+          // }}
           style={{ margin: "4rem auto 0 auto" }}
         >
           Done
